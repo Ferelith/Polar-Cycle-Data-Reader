07db7662908f01e2b95a43e17aa0b1471f49db60
Artur <artur.060969@gmail.com>
2018-02-24 01:24:34 +0000

    Added first form and second form.
    Added first form for file selection and second form to display the contents of the file chosen. Next upload will consist of adding functionality to file selector to open a new form with some data being displayed.

---
+++ Cycle Data Project/Cycle Data Project/Cycle Data Project/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cycle_Data_Project
+{
+    public partial class Form1 : Form
+    {
+        Cycle ccl = new Cycle();
+        Form2 frm2 = new Form2();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Disable textbox1 (select file to load) for file choosing only. 
+            textBox1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Close the current form.
+            // this.Hide();
+            frm2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Close the entire application.
+            Application.Exit();
+        }
+    }
+
+    public class Cycle
+    {
+        public int get_data()
+        {
+            return 0;
+        }
+    }
+}
